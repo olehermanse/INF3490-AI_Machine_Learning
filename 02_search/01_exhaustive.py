2e82588d273e405b1873f0d8ca8e984e67f2e6c4
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2016-09-08 00:37:20 +0200

    Week 2 changes

--- 02_search/01_exhaustive.py
+++ 02_search/01_exhaustive.py
@@ -8,26 +8,34 @@ import matplotlib.pyplot as plt
 def f(x):
     return -x**4 + 2 * x**3 + 2 * x**2 - x
 
-def exhaustive(function, start, stop, step):
+def exhaustive(function, start, stop, step, plotting=False):
     x = start
-    best = (x, function(x))
+    best = (x,-99999)
     while x < stop:
         y = function(x)
         if y > best[1]:
             best = (x,y)
+            if plotting:
+                plt.plot(x,y, color="blue", marker="s", markersize=3)
+        else:
+            if plotting:
+                plt.plot(x,y, color="red", marker="s", markersize=3)
         x += step
-        plt.plot(x,y, color="red", marker="s", markersize=3)
     return best
 
 def plot_with_max(func,start,stop,steps):
     x = np.linspace(start, stop, steps)
 
+    fig = plt.figure("INF3490 - Exhaustive Search")
+    fig.suptitle("Visualization of exhaustive search")
+
     plt.plot(x,func(x))
     step = (stop - start) / steps
-    maximum = exhaustive(func, start, stop, step)
+    maximum = exhaustive(func, start, stop, step, plotting=True)
     (x,y) = maximum
     plt.plot(x,y, color="yellow", marker="*", markersize=16)
 
+    plt.savefig("plot.eps", format="eps")
     plt.show()
 
 if __name__ == "__main__":
