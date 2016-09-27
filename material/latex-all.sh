25e1fd68642806488c8b2072f0e67c2b47fd4f83
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2016-09-28 01:24:10 +0200

    Made latex-all script find files automatically.

--- material/latex-all.sh
+++ material/latex-all.sh
@@ -5,27 +5,20 @@ function triplelatex {
     if [ -d "./.latex/_minted-"$filename ]; then
        mv "./.latex/_minted-"$filename ./
     fi
-    pdflatex -shell-escape -output-directory "./.latex" "$1"
-    pdflatex -shell-escape -output-directory "./.latex" "$1"
-    pdflatex -shell-escape -output-directory "./.latex" "$1"
+    pdflatex -shell-escape -interaction batchmode -output-directory "./.latex" "$1"
+    pdflatex -shell-escape -interaction batchmode -output-directory "./.latex" "$1"
+    pdflatex -shell-escape -interaction batchmode -output-directory "./.latex" "$1"
     mv ./.latex/*.pdf ./
     mv _minted-$filename ./.latex
 }
 
-cd assignment1
-triplelatex as1.tex
-cd ../week1
-triplelatex ex1.tex
-triplelatex sol1.tex
-cd ../week2
-triplelatex ex2.tex
-triplelatex sol2.tex
-cd ../week3
-triplelatex ex3.tex
-triplelatex sol3.tex
-cd ../week4
-triplelatex ex4.tex
-triplelatex sol4.tex
-cd ../week5
-triplelatex ex5.tex
-triplelatex sol5.tex
+for foldername in *; do
+    if [[ -d $foldername ]]; then
+        cd $foldername
+        for filename in *.tex; do
+            triplelatex $filename
+        done
+        cd ..
+    fi
+
+done
