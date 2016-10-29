664994e27b3346ed7d6469536d16fa09f097bcc6
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2016-10-30 01:37:33 +0200

    Added clarification and comments to precode.

--- material/assignment2/code/mlp.py
+++ material/assignment2/code/mlp.py
@@ -1,30 +1,27 @@
-''' Feel free to use numpy for matrix multiplication and
-	other neat features.
-	You can write some helper functions to
-	place some calculations outside the other functions
-	if you like to.
-
-	This pre-code is a nice starting point, but you can
-	change it to fit your needs.
+'''
+    This pre-code is a nice starting point, but you can
+    change it to fit your needs.
 '''
 import numpy as np
 
 class mlp:
-	def __init__(self, inputs, targets, nhidden):
-		self.beta = 1
-		self.eta = 0.1
-		self.momentum = 0.0
+    def __init__(self, inputs, targets, nhidden):
+        self.beta = 1
+        self.eta = 0.1
+        self.momentum = 0.0
 
-		print('To be implemented')
+        print('To be implemented')
 
-	def earlystopping(self, inputs, targets, valid, validtargets):
-		print('To be implemented')
+    # You should add your own methods as well!
 
-	def train(self, inputs, targets, iterations=100):
-		print('To be implemented')
+    def earlystopping(self, inputs, targets, valid, validtargets):
+        print('To be implemented')
 
-	def forward(self, inputs):
-		print('To be implemented')
+    def train(self, inputs, targets, iterations=100):
+        print('To be implemented')
 
-	def confusion(self, inputs, targets):
-		print('To be implemented')
+    def forward(self, inputs):
+        print('To be implemented')
+
+    def confusion(self, inputs, targets):
+        print('To be implemented')
--- material/assignment2/code/movements.py
+++ material/assignment2/code/movements.py
@@ -1,17 +1,29 @@
+#!/usr/bin/env Python3
+'''
+    This file will read in data and start your mlp network.
+    You can leave this file mostly untouched and do your
+    mlp implementation in mlp.py.
+'''
 # Feel free to use numpy in your MLP if you like to.
 import numpy as np
 import mlp
 
-filename = 'data/movements_day1-3.dat'
+filename = '../data/movements_day1-3.dat'
 
 movements = np.loadtxt(filename,delimiter='\t')
-movements[:,:40] = movements[:,:40]-movements[:,:40].mean(axis=0)
+
+# Subtract arithmetic mean for each sensor. We only care about how it varies:
+movements[:,:40] = movements[:,:40] - movements[:,:40].mean(axis=0)
+
+# Find maximum absolute value:
 imax = np.concatenate(  ( movements.max(axis=0) * np.ones((1,41)) ,
                           np.abs( movements.min(axis=0) * np.ones((1,41)) ) ),
-                        axis=0 ).max(axis=0)
+                          axis=0 ).max(axis=0)
+
+# Divide by imax, values should now be between -1,1
 movements[:,:40] = movements[:,:40]/imax[:40]
 
-# Split into training, validation, and test sets
+# Generate target vectors for all inputs 2 -> [0,1,0,0,0,0,0,0]
 target = np.zeros((np.shape(movements)[0],8));
 for x in range(1,9):
     indices = np.where(movements[:,40]==x)
@@ -23,17 +35,31 @@ np.random.shuffle(order)
 movements = movements[order,:]
 target = target[order,:]
 
+# Split data into 3 sets
+
+# Training updates the weights of the network and thus improves the network
 train = movements[::2,0:40]
-traint = target[::2]
+train_targets = target[::2]
 
+# Validation checks how well the network is performing and when to stop
 valid = movements[1::4,0:40]
-validt = target[1::4]
+valid_targets = target[1::4]
 
+# Test data is used to evaluate how good the completely trained network is.
 test = movements[3::4,0:40]
-testt = target[3::4]
+test_targets = target[3::4]
 
-# Train the network
+# Try networks with different number of hidden nodes:
 hidden = 12
-net = mlp.mlp(train,traint,hidden)
-net.earlystopping(train, traint, valid, validt)
-net.confusion(test,testt)
+
+# Initialize the network:
+net = mlp.mlp(train, train_targets, hidden)
+
+# Run training:
+net.earlystopping(train, train_targets, valid, valid_targets)
+# NOTE: You can also call train method from here,
+#       and make train use earlystopping method.
+#       This is a matter of preference.
+
+# Check how well the network performed:
+net.confusion(test,test_targets)
